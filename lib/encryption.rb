fba2f0bbdaa0bcb5fc0d2771cf6788b9bfc8771d
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-06-13 20:21:06 -0600

    Create encyption class with methods

---
+++ lib/encryption.rb
@@ -0,0 +1,69 @@
+class Encryption
+
+  attr_reader :message, :key, :date, :char_set
+
+  def initialize(message, key, date)
+    @message = message.downcase
+    @key = key
+    @date = date
+    @char_set = ("a".."z").to_a << " "
+  end
+
+  def generate_final_shift
+    keys = Key.new(key)
+    offsets = Offset.new(date)
+    temp = keys.key_shift.map.with_index do |key, index|
+      key + offsets.offset_shift[index]
+    end
+    temp.map do |num|
+      if num < 27
+        num
+      else
+        num % 27
+      end
+    end
+  end
+
+  def split_message
+    @message.split("")
+  end
+
+  def get_index
+    indexed_text = []
+    split_message.each do |letter|
+      if @char_set.include?(letter)
+        indexed_text << @char_set.index(letter)
+      else
+        indexed_text << letter
+      end
+    end
+    indexed_text
+  end
+
+  def sliced
+    sliced_text = []
+    get_index.each_slice(4).each do |slice|
+      sliced_text << slice
+    end
+    sliced_text
+  end
+
+  def encode
+    encoded_message = []
+    sliced.map do |slice|
+      slice.each.with_index do |num, index|
+        if !num.is_a?(Integer)
+          encoded_message << num
+        else
+          num += (generate_final_shift[index].to_i - 27)
+          encoded_message << @char_set[num]
+        end
+      end
+    end
+    encoded_message
+  end
+
+  def encrypted_text
+    encode.join
+  end
+end
