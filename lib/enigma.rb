a76046499d491d4b3bc8e1d7e5005a4eced7efa7
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-06-14 10:26:34 -0600

    Created enigma class and methods..needs update

---
+++ lib/enigma.rb
@@ -0,0 +1,15 @@
+require_relative 'encryption'
+require_relative 'decryption'
+
+class Enigma
+
+  def encrypt(message, key, date)
+    encryption = Encryption.new(message, key, date)
+    encryption.initiate
+  end
+
+  def decrypt(message, key, date)
+    decryption = Decryption.new(message, key, date)
+    decryption
+  end
+end
