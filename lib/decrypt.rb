f4aaf40b0c69275817d5b1960333e6e56f454407
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-06-14 20:51:47 -0600

    Update file io for decrypt

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,12 +1,21 @@
 require_relative 'enigma'
-
+require_relative 'key'
+require_relative 'offset'
+require_relative 'encryption'
+require_relative 'decryption'
 
 handle = File.open(ARGV[0], "r")
 incoming = handle.read
-enigma = Enigma.new
-decrypted = enigma.decrypt(incoming)
+handle.close
+
+key = Key.new(ARGV[2])
+date = Offset.new(ARGV[3])
+encrypt = Encryption.new('Hello', key, date)
+decrypt = Decryption.new(incoming, key, date)
+enigma = Enigma.new(key, date, encrypt, decrypt)
 
 writer = File.open(ARGV[1], "w")
-write.write(decrypted[:decryption])
+writer.write(decrypt.decode)
 writer.close
-puts "Create '#{ARGV[1]}' with the key #{decrypted[:key]} and the date #{decrypted[:date]}"
+
+puts "Create '#{ARGV[1]}' with the key #{key.key} and the date #{date.date}"
