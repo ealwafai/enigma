da9bfdfb27e350087f06b42b7ea50f76e43b54a6
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-06-14 20:44:22 -0600

    Update tests for encrypt and decrypt

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,9 +1,15 @@
 require_relative 'spec_helper'
 require_relative '../lib/enigma'
+require 'date'
+
 
 RSpec.describe Enigma do
   before :each do
-    @enigma = Enigma.new
+    @key = Key.new("02715")
+    @date = Offset.new("040895")
+    @encryption = Encryption.new("hello world", @key, @date)
+    @decryption = Decryption.new(@encryption.encode, @key, @date)
+    @enigma = Enigma.new(@key, @date, @encryption, @decryption)
   end
 
   describe 'Instantiation' do
@@ -11,15 +17,21 @@ RSpec.describe Enigma do
       expect(@enigma).to be_a(Enigma)
     end
   end
-
+  it 'has attributes' do
+    expect(@enigma.key.class).to eq(Key)
+    expect(@enigma.date.class).to eq(Offset)
+    expect(@enigma.encryption.class).to eq(Encryption)
+    expect(@enigma.decryption.class).to eq(Decryption)
+  end
   describe 'Methods' do
     it 'encrypts message' do
+
       expected = {
                   encryption: "keder ohulw",
                   key: "02715",
                   date: "040895"
                   }
-      expect(@enigma.encrypt).to eq(expected)
+      expect(@enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
     end
 
     it 'decrypts message' do
@@ -28,7 +40,7 @@ RSpec.describe Enigma do
                   key: "02715",
                   date: "040895"
                   }
-      expect(@enigma.decrypt).to eq(expected)
+      expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to eq(expected)
     end
   end
 end
