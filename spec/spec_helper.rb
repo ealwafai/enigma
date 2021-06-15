091f158b5c3d5069931722f4b2d3c480ff3c8b70
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-06-14 20:45:16 -0600

    Update spec helper to include only lib files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
+require 'rspec'
 require 'simplecov'
 SimpleCov.start
+SimpleCov.add_filter ['spec']
+require './lib/key'
+require './lib/offset'
+require './lib/encryption'
+require './lib/decryption'
+require './lib/enigma'
