813e500553a1951f3074f2d6db473c9b9ea55735
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-06-14 20:46:34 -0600

    Update the encrypt and decrypt methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,13 +3,20 @@ require_relative 'decryption'
 
 class Enigma
 
-  def encrypt(message, key, date)
-    encryption = Encryption.new(message, key, date)
-    encryption.initiate_encryption
+  attr_reader :key, :date, :encryption, :decryption
+
+  def initialize(key = Key.new, date = Offset.new, encryption, decryption)
+    @key = key
+    @date = date
+    @encryption = encryption
+    @decryption = decryption
   end
 
-  def decrypt(message, key, date)
-    decryption = Decryption.new(message, key, date)
-    decryption.initiate_decryption
+  def encrypt(message = @encryption.message, key = @key, date = @date)
+    return {encryption: @encryption.encode, key: @key.key, date: @date.date}
+  end
+
+  def decrypt(message = @decryption.message, key = @key, date = @date)
+    return {decryption: @decryption.decode, key: @key.key, date: @date.date}
   end
 end
