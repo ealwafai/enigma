63d96fb80cbbf3a2c96b927f939721f0c4894352
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-06-14 20:51:41 -0600

    Update file io for encrypt

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,11 +1,21 @@
 require_relative 'enigma'
+require_relative 'key'
+require_relative 'offset'
+require_relative 'encryption'
+require_relative 'decryption'
 
 handle = File.open(ARGV[0], "r")
 incoming = handle.read
-enigma = Enigma.new
-encrypted = enigma.encrypt(incoming)
+handle.close
+
+key = Key.new('12345')
+date = Offset.new('240885')
+encrypt = Encryption.new(incoming, key, date)
+decrypt = Decryption.new(encrypt.encode, key, date)
+enigma = Enigma.new(key, date, encrypt, decrypt)
 
 writer = File.open(ARGV[1], "w")
-write.write(encrypted[:encryption])
+writer.write(enigma.encrypt[:encryption])
 writer.close
-puts "Create '#{ARGV[1]}' with the key #{encrypted[:key]} and the date #{encrypted[:date]}"
+
+puts "Create '#{ARGV[1]}' with the key #{key.key} and the date #{date.date}"
